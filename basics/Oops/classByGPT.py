d13c465d529e0c14846a7400b574d7c62ea8d831
RazaNaqsh <owaisraza1704@gmail.com>
2024-06-19 19:49:57 +0530

    car inheritance

--- basics/Oops/classByGPT.py
+++ basics/Oops/classByGPT.py
@@ -10,7 +10,36 @@ class Car:
     
     def read_odometer(self):
         print(f'This cas has {self.odometer_reading} miles on it')
+        
+    def update_odometer(self,mileage):
+        self.odometer_reading = mileage
     
-car1 = Car('Toyota','Corolla',2024,40)
-print(car1.get_description())
-car1.read_odometer()
+    def increment_odometer(self,miles):
+        self.odometer_reading += miles
+    
+    
+class ElectricCar(Car):
+    def __init__(self, make, model, year, odometer_reading=0,battery_size=75):
+        super().__init__(make, model, year, odometer_reading)
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        print(f'This car has a {self.battery_size}-kWh battery')
+    
+    def get_description(self):
+        return f'{self.year} {self.make} {self.model} (Electric)'
+    
+
+
+# car1 = Car('Toyota','Corolla',2024,40)
+
+# car1.update_odometer(50)
+# car1.read_odometer()
+# car1.increment_odometer(20)
+# print(car1.get_description())
+# car1.read_odometer()
+
+ev1 = ElectricCar('Tesla','Model-5',2024,80,100)
+
+print(ev1.get_description())
+ev1.describe_battery()
