d2e0c222f739cfd3c31e549f09e61d46c56984c7
RazaNaqsh <owaisraza1704@gmail.com>
2024-06-19 20:03:52 +0530

    Format code

--- basics/Oops/classByGPT.py
+++ basics/Oops/classByGPT.py
@@ -1,43 +1,43 @@
 class Car:
-    def __init__(self,make,model,year,odometer_reading=0):
+    def __init__(self, make, model, year, odometer_reading=0):
         self.make = make
-        self.model = model 
+        self.model = model
         self.year = year
-        self.odometer_reading = odometer_reading
-        
+        self.__odometer_reading = odometer_reading  # private attribute
+
     def get_description(self):
-        return (f'{self.year} {self.make} {self.model}')
-    
+        return f"{self.year} {self.make} {self.model}"
+
     def read_odometer(self):
-        print(f'This cas has {self.odometer_reading} miles on it')
-        
-    def update_odometer(self,mileage):
-        self.odometer_reading = mileage
-    
-    def increment_odometer(self,miles):
+        print(f"This cas has {self.odometer_reading} miles on it")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.__odometer_reading:
+            self.__odometer_reading = mileage
+
+    def increment_odometer(self, miles):
         self.odometer_reading += miles
-        
+
     def fill_gas_tank(self):
         print("Filling the gas tank")
-    
-    
+
+
 class ElectricCar(Car):
-    def __init__(self, make, model, year, odometer_reading=0,battery_size=75):
+    def __init__(self, make, model, year, odometer_reading=0, battery_size=75):
         super().__init__(make, model, year, odometer_reading)
         self.battery_size = battery_size
-    
+
     def describe_battery(self):
-        print(f'This car has a {self.battery_size}-kWh battery')
-    
+        print(f"This car has a {self.battery_size}-kWh battery")
+
     def get_description(self):
-        return f'{self.year} {self.make} {self.model} (Electric)'
-    
+        return f"{self.year} {self.make} {self.model} (Electric)"
+
     def fill_gas_tank(self):
         print("Electric car dont have gas tanks")
-    
 
 
-car1 = Car('Toyota','Corolla',2024,40)
+car1 = Car("Toyota", "Corolla", 2024, 40)
 
 # car1.update_odometer(50)
 # car1.read_odometer()
@@ -46,7 +46,7 @@ print(car1.get_description())
 car1.fill_gas_tank()
 # car1.read_odometer()
 
-ev1 = ElectricCar('Tesla','Model-5',2024,80,100)
+ev1 = ElectricCar("Tesla", "Model-5", 2024, 80, 100)
 
 print(ev1.get_description())
 ev1.describe_battery()
