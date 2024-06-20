aee6435b7ace08f10816e01646a8aaa0e1e4dc8d
RazaNaqsh <owaisraza1704@gmail.com>
2024-06-20 18:23:52 +0530

    Lambda exercise

--- basics/FunctionalProgramming/lambdaFunction.py
+++ basics/FunctionalProgramming/lambdaFunction.py
@@ -1,19 +1,42 @@
 from functools import reduce
 
-my_list = [1, 2, 3]
+# my_list = [1, 2, 3]
 
-# This lambda function is not stored anywhere, but just used once
-# One time anonymous function
+# # This lambda function is not stored anywhere, but just used once
+# # One time anonymous function
 
-# Syntax:
-# lambda param : action(param)
-print(list(map(lambda item: item * 2, my_list)))
+# # Syntax:
+# # lambda param : action(param)
+# print(list(map(lambda item: item * 2, my_list)))
 
 
-# Filter using lambda function
-# Odd numbers only
-print(list(filter(lambda item: item % 2 != 0, my_list)))
+# # Filter using lambda function
+# # Odd numbers only
+# print(list(filter(lambda item: item % 2 != 0, my_list)))
 
-# Reduce using lambda
-# reduce(function, data,starting point)
-print(reduce(lambda acc, item: acc + item, my_list, 10))
+# # Reduce using lambda
+# # reduce(function, data,starting point)
+# print(reduce(lambda acc, item: acc + item, my_list, 10))
+
+
+# # Exercise
+# # Print squared list
+
+# print(list(map(lambda item: item**2, my_list)))
+
+
+# List sorting acc to 2nd element
+
+a = [(0, 2), (4, 3), (10, -1), (9, 9)]
+
+# If we simply sort a
+
+a.sort()  # sorts on the basis of first key
+print(a)
+
+
+# Sorting on the basis of 2nd key
+
+a.sort(key=lambda x: x[1])
+
+print(a)
