89de2b9c7997e3901d8c27bc9976dc15a091f6c5
RazaNaqsh <owaisraza1704@gmail.com>
2024-06-21 13:50:21 +0530

    Custom decorators

---
+++ basics/FunctionalProgramming/ddecorators.py
@@ -0,0 +1,38 @@
+# Decorator
+def my_decorator(func):
+    def wrap_func():
+        print("*****")
+        func()
+        print("*****")
+
+    return wrap_func
+
+
+@my_decorator
+def hello():
+    print("helloo")
+
+
+# hello()
+
+from time import time
+
+
+def performance(fn):
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = fn(*args, **kwargs)
+        t2 = time()
+        print(f"took {t2-t1} s")
+        return result
+
+    return wrapper
+
+
+@performance
+def long_time():
+    for i in range(10000000):
+        i * 5
+
+
+long_time()
