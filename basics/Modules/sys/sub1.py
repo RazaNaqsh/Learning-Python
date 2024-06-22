8893991a7e097653649c704b8ebe62d776427be0
RazaNaqsh <owaisraza1704@gmail.com>
2024-06-22 19:13:08 +0530

    Sys module

---
+++ basics/Modules/sys/sub1.py
@@ -0,0 +1,9 @@
+import sys
+
+first = sys.argv[1]
+second = sys.argv[2]
+
+print(f"h1 {first} {second}")
+
+# If  we call this file using (python sub1.py Owais Raza)
+# we can catch the args using argv[1] and 2, then i can print
