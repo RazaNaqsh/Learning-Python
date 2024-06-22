40d29c3c7d5a34a4a3fe4de41717e7a91aaf9535
RazaNaqsh <owaisraza1704@gmail.com>
2024-06-22 14:34:34 +0530

    creating and using package

--- basics/Modules/main.py
+++ basics/Modules/main.py
@@ -1,5 +1,12 @@
-import utility
+import utility  # imports the module
+import shopping.shopping_cart  # importing package
+
+# shopping is the package , the folder
+# the file inside is the module
 
 utility.test()
 
 print(utility.multiply(2, 3))
+
+
+print(shopping.shopping_cart.buy("apple"))
