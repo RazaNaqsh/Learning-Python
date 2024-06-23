2dcb8d222c6f6a406ec5e44243abdbcb874af042
RazaNaqsh <owaisraza1704@gmail.com>
2024-06-23 14:21:31 +0530

    file io seek

--- basics/FileIO/script.py
+++ basics/FileIO/script.py
@@ -1,3 +1,15 @@
 my_file = open("test.txt")
 
 print(my_file.read())
+
+# What the above code does is that it opens the file and then we read
+# When reading the cursor moves at the end and if we try to print read again
+
+print(my_file.read())  # this doesnt work , since the cursor is at the end
+
+# To work this out we use seek function
+
+my_file.seek(0)  # Now we are at 0 means first
+print(my_file.read())
+
+# Thus the output we get is that first print works 2nd dont work then third works
