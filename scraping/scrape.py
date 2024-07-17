5fe341fa8b3ee43b627399fe4668360522cebc4c
RazaNaqsh <owaisraza1704@gmail.com>
2024-07-17 12:03:29 +0530

    scraping articles with point over 100

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -1,12 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 
 res = requests.get("https://news.ycombinator.com/news")
 # print(res.text)
 soup = BeautifulSoup(res.text, "html.parser")
 
-# print(soup.find_all("a"))  # finds all the a tag
-# print(soup.find("a"))  # finds the first item
-print(soup.find(id="idOfAnyTag"))  # finds the unique ele with the id
-print(soup.select(".score"))  # selects with the css selectors
+links = soup.select(".titleline a")
+subtext = soup.select(".subtext")
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        if idx < len(subtext):  # Ensure we do not go out of bounds
+            title = item.getText()
+            href = item.get("href", None)
+            vote = subtext[idx].select(".score")
+            if vote:  # Check if the vote element exists
+                points = int(vote[0].getText().replace(" points", ""))
+                if points > 100:  # Check if points are greater than 0
+                    hn.append({"title": title, "link": href, "votes": points})
+    return hn
+
+
+pprint.pprint(create_custom_hn(links, subtext))
+# create_custom_hn(links, subtext)
---
+++ scraping/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+if len(vote):
+        #     points = int(vote[0].getText().replace(" points", ""))
+        #     print(points)
+        #     hn.append({"title": title, "link": href})
